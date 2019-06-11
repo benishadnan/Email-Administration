305e807164bfc87a33d6ad730b3d6ed6aade597e
Adnan Gohar <adnan.gohar@rebtel.com>
2019-06-11 15:50:12 +0200

    basic structure of email administration application

---
+++ emailAdministration/src/emailapp/Email.java
@@ -0,0 +1,24 @@
+package emailapp;
+
+public class Email {
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String department;
+	private String alternateEmail;
+	private int mailboxCapacity;
+	
+	// Constructor to receive the first name and last name
+	public Email(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		System.out.println("Email created: " + this.firstName + " "+ this.lastName);
+	}
+	
+	// Ask for department
+	// Generate a random password
+	
+	// Set the mailbox capacity 
+	
+	//Change the password
+}
